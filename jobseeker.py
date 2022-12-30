bc3b164a286a432ab7d88310f4137d1308ffd172
Sergei Shcherbak <sshch1987@gmail.com>
2022-12-30 21:18:14 +0200

    Update jobseeker.py

--- jobseeker.py
+++ jobseeker.py
@@ -15,12 +15,12 @@ def send_email(message):
         return "The message was sent successfully!"
 
     except Exception as _ex:
-        return f"{_ex}\nCheck your login/pass or do whatever you need to fix this bug, please!"
-
+        return f"{_ex}\nSomething went wrong... Please, check your login/password or account settings!"
 
 
 def main():
-    ...
+    message = input("Type in your message here:  ")
+    print(send_email(message=message))
 
 
 if __name__ == '__main__':
