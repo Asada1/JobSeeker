49b9632bf602729a0f6b1bc954a0512c81c1f2da
Sergei Shcherbak <sshch1987@gmail.com>
2022-12-30 20:51:25 +0200

    Create jobseeker.py

---
+++ jobseeker.py
@@ -0,0 +1,27 @@
+import smtplib
+
+
+def send_email(message):
+    sender = "your.email@isegal.de"
+    password = "##whatever**"
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+
+    try:
+        server.login(sender, password)
+        server.sendmail(sender, sender, message)
+
+        return "The message was sent successfully!"
+
+    except Exception as _ex:
+        return f"{_ex}\nCheck your login/pass or do whatever you need to fix this bug, please!"
+
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
