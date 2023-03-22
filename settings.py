509bd75223587d424c16f5fb41ad3dbfa1582e9a
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-22 16:37:08 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -17,8 +17,7 @@ def get_key():
 
 def get_settings():
     sender = get_sender()
-    password = "##whatever**"
-
+    password = get_key()
     server = smtplib.SMTP("smtp.gmail.com", 587)
 
     setting_tuple = (sender, password, server)
