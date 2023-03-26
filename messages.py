e77eb5ee540d686d0516879b521b0a7fa3f8516d
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-26 20:54:36 +0300

    aktualisiert

--- messages.py
+++ messages.py
@@ -1,15 +1,15 @@
 class Message:
     def get_message(self):
-        with open('keybox/texts.txt', 'r') as texts:
+        with open('tools/texts.txt', 'r') as texts:
             text = texts.read()
         return text
 
     def get_subject(self):
-        with open('keybox/subjects.txt', 'r') as subjects:
+        with open('tools/subjects.txt', 'r') as subjects:
             subject = subjects.read()
         return subject
 
     def get_receiver(self):
-        with open('keybox/receivers.txt', 'r') as receivers:
+        with open('tools/receivers.txt', 'r') as receivers:
             receiver = receivers.read()
         return receiver
