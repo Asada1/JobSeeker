74cbc5dbc303e049000e0760cbf6139dbf374c9f
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-26 17:47:14 +0300

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,22 +1,23 @@
 import texts
 
 
-def get_message():
-    message = texts.compose_my_letter()
+class Message:
+    def __init__(self, message, subject, receiver):
+        self.message = message
+        self.subject = subject
+        self.receiver = receiver
 
-    return message
+    def get_message():
+        message = texts.compose_my_letter()
+        return message
 
+    def get_subject():
+        subject = texts.make_my_header()
+        return subject
 
-def get_subject():
-    subject = texts.make_my_header()
-
-    return subject
-
-
-def get_receiver():
-    receiver = texts.get_receiver()
-
-    return receiver
+    def get_receiver():
+        receiver = texts.get_receiver()
+        return receiver
 
 
 def get_header():
