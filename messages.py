7ae40368903ed80b0c4aaa1dd9ff1c674b20c0e6
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-26 17:50:40 +0300

    Update messages.py

--- messages.py
+++ messages.py
@@ -7,15 +7,15 @@ class Message:
         self.subject = subject
         self.receiver = receiver
 
-    def get_message():
+    def get_message(self):
         message = texts.compose_my_letter()
         return message
 
-    def get_subject():
+    def get_subject(self):
         subject = texts.make_my_header()
         return subject
 
-    def get_receiver():
+    def get_receiver(self):
         receiver = texts.get_receiver()
         return receiver
 
