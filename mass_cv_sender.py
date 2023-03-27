c8021bda59737fa00d67a4f64fc43102f6b0fe1f
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-27 17:42:55 +0300

    Update mass_cv_sender.py

--- mass_cv_sender.py
+++ mass_cv_sender.py
@@ -9,25 +9,25 @@ def get_set_messages():
     ...
 
 
-    def send_email(message):
-        sender = settings.get_sender()
-        password = settings.get_key()
-        server = smtplib.SMTP("smtp.gmail.com", 587)
-        server.starttls()
+def send_email(message):
+    sender = settings.get_sender()
+    password = settings.get_key()
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
 
-        try:
-            tolist = messages.get_receiver()
-            server.login(sender, password)
-            message = MIMEText(message, 'plain', 'utf-8')
-            message["Subject"] = messages.get_subject()
-            message["From"] = sender
-            message["To"] = ', '.join(tolist)
-            server.sendmail(sender, tolist, message.as_string())
+    try:
+        tolist = messages.get_receiver()
+        server.login(sender, password)
+        message = MIMEText(message, 'plain', 'utf-8')
+        message["Subject"] = messages.get_subject()
+        message["From"] = sender
+        message["To"] = ', '.join(tolist)
+        server.sendmail(sender, tolist, message.as_string())
 
-            return "The message was sent successfully!"
+        return "The message was sent successfully!"
 
-        except Exception as _ex:
-            return f"{_ex}\nSomething went wrong... Please, check your settings!"
+    except Exception as _ex:
+        return f"{_ex}\nSomething went wrong... Please, check your settings!"
 
 
 def main():
