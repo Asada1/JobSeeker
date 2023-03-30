3f6841189ede2e71d4003c364536c8c2ddc59e8f
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-30 13:18:35 +0300

    Update standard_message_sender.py

--- standard_message_sender.py
+++ standard_message_sender.py
@@ -1,5 +1,10 @@
+import mimetypes
+import os
 import smtplib
-from email.mime.text import MIMEText
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from tqdm import tqdm
 
 import settings
 import standard_message as sm
@@ -14,10 +19,23 @@ def send_email(message):
     try:
         tolist = sm.get_receiver()
         server.login(sender, password)
-        message = MIMEText(message, 'plain', 'utf-8')
+        # message = MIMEText(message, 'plain', 'utf-8')
+        message = MIMEMultipart()
         message["Subject"] = sm.get_subject()
         message["From"] = sender
         message["To"] = ', '.join(tolist)
+
+        for file in tqdm(os.listdir("attachments")):
+            filename = os.path.basename(file)
+            ftype, encoding = mimetypes.guess_type(file)
+            file_type, subtype = ftype.split("/")
+            with open(f"attachments/{file}", "rb") as f:
+                file = MIMEBase(file_type, subtype)
+                file.set_payload(f.read())
+                encoders.encode_base64(file)
+            file.add_header('content-disposition', 'attachment', filename=filename)
+            message.attach(file)
+
         server.sendmail(sender, tolist, message.as_string())
 
         return "The message was sent successfully!"
