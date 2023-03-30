334e939a9b9bdd53f90b175006fdd0e1184d2af0
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-30 10:54:45 +0300

    Update standard_message.py

--- standard_message.py
+++ standard_message.py
@@ -23,25 +23,3 @@ def get_receiver():
     with open('email_stuff/standard/receivers.txt', 'r') as receivers:
         receiver = receivers.readlines()
     return receiver
-
-
-def get_file():
-    for file in tqdm(os.listdir("attachments")):
-        ftype, encoding = mimetypes.guess_type(file)
-        file_type, subtype = ftype.split("/")
-
-        if file_type == "text":
-            with open(f"attachments/{file}") as f:
-                file = MIMEText(f.read())
-
-        elif file_type == "application":
-            with open(f"attachments/{file}", "rb") as f:
-                file = MIMEApplication(f.read(), subtype)
-
-        else:
-            with open(f"attachments/{file}", "rb") as f:
-                file = MIMEBase(file_type, subtype)
-                file.set_payload(f.read())
-                encoders.encode_base64(file)
-
-    return file
