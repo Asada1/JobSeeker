5d3a183cd2bc01077e2d594551d6a6c497dc3e67
Sergei Shcherbak <sshch1987@gmail.com>
2023-03-30 11:37:24 +0300

    Update standard_message.py

--- standard_message.py
+++ standard_message.py
@@ -1,12 +1,3 @@
-import os
-import mimetypes
-from tqdm import tqdm
-from email import encoders
-from email.mime.text import MIMEText
-from email.mime.application import MIMEApplication
-from email.mime.base import MIMEBase
-
-
 def get_message():
     with open('email_stuff/standard/message_text.txt', 'r') as message_text:
         message = message_text.read()
