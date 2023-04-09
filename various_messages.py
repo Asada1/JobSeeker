ee009cf93bf0c74bdd6a10cb400a07b8b7f9081f
Sergei Shcherbak <sshch1987@gmail.com>
2023-04-09 18:03:15 +0300

    Update various_messages.py

--- various_messages.py
+++ various_messages.py
@@ -1,16 +1,16 @@
 def mass_message():
-    with open('email/various/message_texts.txt', 'r') as message_texts:
+    with open('email/various/message_texts.txt', 'r', encoding='utf-8') as message_texts:
         message = message_texts.readlines()
     return message
 
 
 def mass_subject():
-    with open('email/various/message_subjects.txt', 'r') as letter_subjects:
+    with open('email/various/message_subjects.txt', 'r', encoding='utf-8') as letter_subjects:
         subject = letter_subjects.readlines()
     return subject
 
 
 def mass_receiver():
-    with open('email/various/receivers.txt', 'r') as receivers:
+    with open('email/various/receivers.txt', 'r', encoding='utf-8') as receivers:
         receiver = receivers.readlines()
     return receiver
