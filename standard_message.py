deb834e8bab96629b1f677074b3ebf9d348ae9f7
Sergei Shcherbak <sshch1987@gmail.com>
2023-04-09 18:45:38 +0300

    Update standard_message.py

--- standard_message.py
+++ standard_message.py
@@ -7,11 +7,12 @@ def get_message():
 def get_subject():
     with open('email/standard/message_subject.txt', 'r', encoding='utf-8') as letter_subject:
         subject = letter_subject.read()
+        
     return subject
 
 
 def get_receiver():
     with open('email/standard/receivers.txt', 'r', encoding='utf-8') as receivers:
         receiver = receivers.readlines()
-        
+
     return receiver
